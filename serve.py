bdafbc01801eb74bc3ce414c13ba5f6ec4c584e3
Francisco Gonzalez <fjgonza2@illinois.edu>
2019-07-01 18:53:51 -0500

    Return model api

--- serve.py
+++ serve.py
@@ -1,4 +1,42 @@
-# Import model
+import pickle
+
+import tensorflow as tf
+
+from model.src import main
+from model.src.utils import utils
+from model.src.utils import data_utils
+
+
+def process_input_data(input_data, tokenizer):
+    """Process input data for model to use."""
+    # Clean raw text
+    text = data_utils.text_to_wordlist(input_data)
+
+    # Tokenize and pad text
+    padded_token_seq = data_utils.get_padded_sequences(tokenizer, [text])
+
+    return padded_token_seq
+
+
+def process_output_data(pred_gen, padded_token_seq, tokenizer):
+    """Process output data."""
+    # Yield prediction dictionary from generator
+    pred_dict = next(pred_gen)
+
+    # Regenerate text
+    text = [tokenizer.index_word[idx] for idx in padded_token_seq[0] if idx != 0]
+
+    # Normalize alphas for highlighting text
+    mask = padded_token_seq[0] != 0
+    alphas = (pred_dict["alphas"] / pred_dict["alphas"].max())[mask]
+
+    output_data = {
+        "class": pred_dict["class_ids"][0],
+        "probability": pred_dict["probabilities"][0],
+        "text": text,
+        "alphas": alphas,
+    }
+    return output_data
 
 
 def get_model_api():
@@ -8,33 +46,39 @@ def get_model_api():
         type: Description of returned object.
 
     """
+    # Load hyperparameters and tokenizer
+    with open("data/tokenizer.pkl", "rb") as f:
+        tokenizer = pickle.load(f)
+    params = utils.load_config("src/config.yml")
+    params.tokenizer = tokenizer
 
-    # Initialize model.
-    # This could be either a pb file or restoration from checkpoints
-    #
-    # model = model.Model()
+    # 1. Initialize model.
+    estimator = tf.estimator.Estimator(
+        model_fn=main.model_fn, params=params, model_dir="data/ckpt/"
+    )
 
     def model_api(input_data):
         """Model API.
 
         Args:
-            input_data (type): Description of parameter `input_data`.
+            input_data: submitted to the API, raw string
 
         Returns:
-            type: Description of returned object.
+            output_data
 
         """
-        # Process input data
-        #
-        # processed_data = process_data(input_data)
+        # 2. Process input data
+        padded_token_seq = process_input_data(input_data, tokenizer)
 
-        # Call model predict function
-        #
-        # predictions = model.predict(precessed_data)
+        # 3. Call model predict function
+        pred_gen = estimator.predict(
+            input_fn=lambda: data_utils.input_fn(
+                features=padded_token_seq, labels=None, batch_size=1, buffer_size=1
+            )
+        )
 
-        # Process the output
-        #
-        # output_data = postprocess_data(predictions)
+        # 4. Process the output
+        output_data = process_output_data(pred_gen, padded_token_seq, tokenizer)
 
         # Return data
         return output_data
